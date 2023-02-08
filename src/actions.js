692c5d95bb087d6b76fb4c2c34fcbb20c39aed0f
EA <erhnaydn1999@gmail.com>
2023-02-08 14:45:28 +0300

    updated

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const GET_FAVS_FROM_LS = "GET_FAVS_FROM_LS";
 export const FAV_ADD = "FAV_ADD";
 export const FAV_REMOVE = "FAV_REMOVE";
@@ -18,4 +20,11 @@ export const removeFav = (id) => {
 }
 
 export const fetchAnother = () => dispatch => {
+  dispatch({type: FETCH_LOADING});
+
+  axios
+    .get("https://dog.ceo/api/breeds/image/random")
+    .then((response) => dispatch({type: FETCH_SUCCESS, payload: {...response.data, id: Date.now()}}))
+    .catch((error) => dispatch({type: FETCH_ERROR, payload: error}));
 }
+
--- src/reducers.js
+++ src/reducers.js
@@ -19,28 +19,52 @@ function writeFavsToLocalStorage(state) {
 }
 
 function readFavsFromLocalStorage() {
+  if (!JSON.parse(localStorage.getItem("s10g4"))) return [];
   return JSON.parse(localStorage.getItem("s10g4"));
 }
 
 export function myReducer(state = initial, action) {
   switch (action.type) {
     case FAV_ADD:
-      return state;
+      writeFavsToLocalStorage([...state.favs, action.payload])
+      let control = state.favs.every((f) => f.message !== action.payload.message);
+      return {
+        ...state,
+        favs: control ? [...state.favs, action.payload] : [...state.favs]
+      }
 
     case FAV_REMOVE:
-      return state;
+      const newFavs = state.favs.filter((item) => item.id !== action.payload);
+      return {
+        ...state,
+        favs: [...newFavs],
+      };
 
     case FETCH_SUCCESS:
-      return state;
+      return {
+        ...state,
+        loading: false,
+        current: action.payload
+      };
 
     case FETCH_LOADING:
-      return state;
+      return {
+        ...state,
+        loading: true
+      };
 
     case FETCH_ERROR:
-      return state;
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      };
 
     case GET_FAVS_FROM_LS:
-      return state;
+      return {
+        ...state,
+        favs: readFavsFromLocalStorage()
+      };
 
     default:
       return state;
